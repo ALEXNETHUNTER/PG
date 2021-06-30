355d20940aed9f78a55c69395a2930c263b91343
Alex <ALEXNETHUNTER@GMAIL.COM>
2021-06-30 16:21:44 +0000

    LReplica

--- LReplica.sh
+++ LReplica.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
 PG_DATA="/home/devops/PG_test/PG"
+U=`whoami`
 ip=$1
 touch $PG_DATA/postgresql.conf
 
 echo "listen_addresses = '$ip'" >> $PG_DATA/postgresql.conf
 echo "max_prepared_transactions = 100" >> $PG_DATA/postgresql.conf
 echo "max_wal_senders = 3" >> $PG_DATA/postgresql.conf
-echo "wal_level = replica" >> $PG_DATA/postgresql.conf
+echo "wal_level = logical" >> $PG_DATA/postgresql.conf
 echo "max_connections = 100" >> $PG_DATA/postgresql.conf
 echo "wal_log_hints = on" >> $PG_DATA/postgresql.conf
 echo "max_wal_senders = 8" >> $PG_DATA/postgresql.conf
+echo "hot_standby = on" >> $PG_DATA/postgresql.conf
 
 touch $PG_DATA/pg_hba.conf
 
-echo "# TYPE   DATABASE         USER            ADDRESS            METHOD" >> $PG_DATA/pg_hba.conf
-echo "host    replication     replication         $ip                 md5" >> $PG_DATA/pg_hba.conf
+echo "# TYPE   DATABASE  USER  ADDRESS            METHOD" >> $PG_DATA/pg_hba.conf
+echo "  host  replication $U    $ip              trust" >> $PG_DATA/pg_hba.conf 
+echo "  host     all      $U    $ip              trust" >> $PG_DATA/pg_hba.conf
 
--- LReplica_Inst.sh
+++ LReplica_Inst.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env bash
 
-PG_DATA="/etc/postgresql/12/main"
+U=`whoami`
 ip=$1
 touch $PG_DATA/postgresql.conf
 
 echo "listen_addresses = '$ip'" >> $PG_DATA/postgresql.conf
 echo "max_prepared_transactions = 100" >> $PG_DATA/postgresql.conf
 echo "max_wal_senders = 3" >> $PG_DATA/postgresql.conf
-echo "wal_level = replica" >> $PG_DATA/postgresql.conf
+echo "wal_level = logical" >> $PG_DATA/postgresql.conf
 echo "max_connections = 100" >> $PG_DATA/postgresql.conf
 echo "wal_log_hints = on" >> $PG_DATA/postgresql.conf
 echo "max_wal_senders = 8" >> $PG_DATA/postgresql.conf
+echo "hot_standby = on" >> $PG_DATA/postgresql.conf
 
 touch $PG_DATA/pg_hba.conf
 
-echo "# TYPE   DATABASE         USER            ADDRESS            METHOD" >> $PG_DATA/pg_hba.conf
-echo "host    replication     replication         $ip                 md5" >> $PG_DATA/pg_hba.conf
+echo "# TYPE   DATABASE  USER  ADDRESS            METHOD" >> $PG_DATA/pg_hba.conf
+echo "  host  replication $U    $ip              trust" >> $PG_DATA/pg_hba.conf
+echo "  host     all      $U    $ip              trust" >> $PG_DATA/pg_hba.conf
 
 systemctl restart postgresql
+
+cat /etc/postgresql/12/main/postgresql.conf > /home/devops/PG_test/PG/postgresql.conf
+
+git add .
+git commit -m "LReplica"
+git push -u origin LReplica
