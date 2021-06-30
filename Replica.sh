85a8f942a4530459e2122182741525b7dc4fe187
Alex <ALEXNETHUNTER@GMAIL.COM>
2021-06-30 10:58:45 +0000

    master

---
+++ Replica.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+PG_DATA="/etc/postgresql/12/main/"
+ip=$1
+touch $PG_DATA/postgresql.conf
+
+echo "listen_addresses = '$1'" >> $PG_DATA/postgresql.conf
+echo "max_prepared_transactions = 100" >> $PG_DATA/postgresql.conf
+echo "max_wal_senders = 3" >> $PG_DATA/postgresql.conf
+echo "wal_level = replica" >> $PG_DATA/postgresql.conf
+echo "max_connections = 100" >> $PG_DATA/postgresql.conf
+echo "wal_log_hints = on" >> $PG_DATA/postgresql.conf
+echo "max_wal_senders = 8" >> $PG_DATA/postgresql.conf
+
+touch $PG_DATA/pg_hba.conf
+
+echo "# TYPE   DATABASE         USER            ADDRESS            METHOD" >> $PG_DATA/pg_hba.conf
+echo "host    replication     replication     10.10.10.1/24         md5" >> $PG_DATA/pg_hba.conf
+
