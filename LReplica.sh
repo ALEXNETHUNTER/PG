b781a89f5392a58114abff6e501f9dc943f95e7c
Alex <ALEXNETHUNTER@GMAIL.COM>
2021-06-30 11:57:24 +0000

    LReplica

--- LReplica.sh
+++ LReplica.sh
@@ -4,7 +4,7 @@ PG_DATA="/home/devops/PG_test/PG"
 ip=$1
 touch $PG_DATA/postgresql.conf
 
-echo "listen_addresses = '$1'" >> $PG_DATA/postgresql.conf
+echo "listen_addresses = '$ip'" >> $PG_DATA/postgresql.conf
 echo "max_prepared_transactions = 100" >> $PG_DATA/postgresql.conf
 echo "max_wal_senders = 3" >> $PG_DATA/postgresql.conf
 echo "wal_level = replica" >> $PG_DATA/postgresql.conf
@@ -15,5 +15,5 @@ echo "max_wal_senders = 8" >> $PG_DATA/postgresql.conf
 touch $PG_DATA/pg_hba.conf
 
 echo "# TYPE   DATABASE         USER            ADDRESS            METHOD" >> $PG_DATA/pg_hba.conf
-echo "host    replication     replication         $1                 md5" >> $PG_DATA/pg_hba.conf
+echo "host    replication     replication         $ip                 md5" >> $PG_DATA/pg_hba.conf
 
